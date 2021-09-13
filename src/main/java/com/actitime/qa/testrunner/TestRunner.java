431d922b23902259ebd8b008142ad01141857d25
sarath-kadigamugedara <52712912+sarath-kadigamugedara@users.noreply.github.com>
2021-09-13 08:28:52 +0530

    Initial commit

---
+++ src/main/java/com/actitime/qa/testrunner/TestRunner.java
@@ -0,0 +1,27 @@
+package com.actitime.qa.testrunner;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features = "/Users/methuliakithma/eclipse-workspace/ActitimeTestBDD/src/main/java/com/actitime/qa/feature/Loging.feature"
+		,glue={"com/actitime/qa/stepdefinitions"}
+		,monochrome = true,
+				strict=true,
+				dryRun=false
+				
+		)
+
+
+
+
+public class TestRunner {
+	
+	
+	
+
+}
