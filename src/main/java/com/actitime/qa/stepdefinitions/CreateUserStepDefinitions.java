65819c2b724ca97ca8618425ad06f66c67fc7eec
ipmanlk <io@navinda.xyz>
2023-03-09 23:15:07 +0530

    fix: step definition and feature file is missing

--- src/main/java/com/actitime/qa/stepdefinitions/CreateUserStepDefinitions.java
+++ src/main/java/com/actitime/qa/stepdefinitions/CreateUserStepDefinitions.java
@@ -1,9 +1,10 @@
 package com.actitime.qa.stepdefinitions;
 
 import com.actitime.qa.base.TestBase;
+import com.actitime.qa.entities.UserCredential;
 import com.actitime.qa.pages.HomePage;
 import com.actitime.qa.pages.UsersPage;
-import com.actitime.qa.util.TestUtil;
+import com.actitime.qa.util.UsersUtil;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -11,23 +12,35 @@ import io.cucumber.java.en.When;
 public class CreateUserStepDefinitions extends TestBase {
     UsersPage usersPage;
     HomePage homePage;
-    String sheetName = "Users";
-    TestUtil testUtil;
 
-
-    @Given("^User in the Actitime Users page$")
-    public void user_in_the_Actitime_Users_Page() throws Throwable {
-
+    @Given("^User in the Actitime home page$")
+    public void user_in_the_Actitime_Home_Page() throws Throwable {
         initialization();
-        usersPage = new UsersPage();
     }
 
-    @When("^User does something$")
-    public void user_does_something() throws Throwable {
+    @When("^User click on users tab$")
+    public void User_click_on_users_tab() throws Throwable {
+        homePage.clickOnUsersLink();
     }
 
-    @Then("^Users section should be visible$")
-    public void user_enter_something() throws Throwable {
+    @Then("^User click on new user button$")
+    public void user_click_on_new_user_button() throws Throwable {
+        usersPage.clickOnNewUserButton();
     }
 
+    @Then("^Fill in user data$")
+    public void user_fill_in_user_data() throws Throwable {
+        UserCredential userCredential = UsersUtil.getUserCredential();
+        usersPage.fillUserData(userCredential);
+    }
+
+    @Then("^Click on save and send invite button$")
+    public void user_click_on_send_invite_button() throws Throwable {
+        usersPage.clickOnSaveAndSendInvitationButton();
+    }
+
+    @Then("^User should get added successfully$")
+    public void user_should_be_added_successfully() throws Throwable {
+        driver.quit();
+    }
 }
